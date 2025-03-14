8445cd8f4a465f098a8919ad2d0b3818b6ddfd21
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-14 15:46:57 -0300

    removing vite default css

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 A place to practice Tailwindcss and improve skills. Used for tests as well in a lab controlled environment.
 
 ## Concepts
+- Utility first (functional css): imutability, previsibility, prevent side effects.
+- Composition x inheritance: avoid rewriting properties and inheritance.
+- Easier to understand what is appplied in a specific element.
--- src/pages/CardSimple/index.jsx
+++ src/pages/CardSimple/index.jsx
@@ -1,14 +1,16 @@
+import "./CardSimple.css";
+
 const CardSimple = () => {
   return (
-    <div class="p-10 m-auto my-10
-      text-black
-      bg-gradient-to-r from-pink-400 to-slate-400 w-200 h-50
+    <div className="p-10 m-auto my-10
+      text-qualquer font-inter
+      bg-gradient-to-r from-pink-400 to-slate-400 w-[200px] h-[50px]
       flex flex-col justify-center items-start
       ">
-      <p class="text-6xl font-black">Gabriela</p>
-      <p class="text-4xl">Desenvolvedora Front-end</p>
+      <p className="text-6xl font-black">Gabriela</p>
+      <p className="text-4xl">Desenvolvedora Front-end</p>
     </div>
-  )
-}
+  );
+};
 
-export default CardSimple
+export default CardSimple;
