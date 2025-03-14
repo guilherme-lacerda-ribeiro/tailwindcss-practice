d9ac979b344710883d8fdd6793333da492384e6b
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-03-14 14:47:33 -0300

    in english

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ function App() {
   return (
       <div class="p-10 m-auto my-10
         text-black
-        bg-gradient-to-r from-pink-400 to-slate-400 w-200 h-50
+        bg-gradient-to-r from-pink-400 to-slate-400 w-250 h-50
         flex flex-col justify-center
+        rounded-4xl
         ">
-        <p class="text-4xl">Bora praticar, acesso via url diretamente</p>
+        <p class="text-4xl">Lab envinonment, access through endpoints directly.</p>
       </div>
   )
 }
